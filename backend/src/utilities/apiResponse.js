498dad579f0763f6c03a24f7696ac3989863b03e
Sushil Simon Tete <sushil.simontete@protonmail.com>
2024-12-09 19:40:04 +0530

    creation of apiReponse class

---
+++ backend/src/utilities/apiResponse.js
@@ -0,0 +1,36 @@
+class ApiResponse {
+    constructor(success, message, data = null, meta = null) {
+      this.success = success;
+      this.message = message;
+      this.data = data;
+      this.meta = meta; 
+    }
+  
+    static success(message, data = null, meta = null) {
+      return new ApiResponse(true, message, data, meta);
+    }
+  
+   error(message, details = null) {
+      return new ApiResponse(false, message, details);
+    }
+
+    toJSON() {
+      const response = {
+        success: this.success,
+        message: this.message,
+      };
+  
+      if (this.data !== null) {
+        response.data = this.data;
+      }
+  
+      if (this.meta !== null) {
+        response.meta = this.meta;
+      }
+  
+      return response;
+    }
+  }
+  
+ export default ApiResponse
+  
