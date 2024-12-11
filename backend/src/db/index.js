661aeb01e063cf73fd23a33779ce8611d1166085
Sushil Simon Tete <sushil.simontete@protonmail.com>
2024-12-11 16:51:26 +0530

    added cors, url extender, cookieparser. edited databaseconnection code.

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,15 +1,27 @@
 import mongoose from "mongoose"
 import  { databaseName }  from "../constants.js"
+import ApiError from "../utilities/apiError.js"
+import ApiResponse from "../utilities/apiResponse.js";
 
 
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODBURI}/${databaseName}`)
-    console.log("Database has been connected successfully")
-  } catch ( error) {
-    console.log("Database connection failed", error)
+    const dataBaseConnect = await mongoose.connect(`${process.env.MONGODBURI}/${databaseName}`);
+    console.log(
+      ApiResponse.success(
+        `Database has been connected successfully at HOST: ${dataBaseConnect.connection.host}`
+      ).toJSON()
+    );
+  } catch (error) {
+    const apiError = new ApiError(
+      500,
+      "Database connection failed",
+      { originalError: error.message }
+    );
+    console.error(apiError);
+    throw apiError; 
   }
-}
+};
 
 export default connectDB
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,3 @@
-import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 
@@ -8,13 +7,10 @@ dotenv.config({
 })
 
 
-const app = express()
+
 
 connectDB()
 
-app.listen(process.env.PORT , ()=>{
-    console.log("app has started")
-})
 
 
 
